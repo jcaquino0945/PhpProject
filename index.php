2b3ee80786060487d59d3086f44ed814d37e00b1
Miguel Aquino <jcaquino0945@gmail.com>
2021-11-10 15:12:00 +0800

    form

---
+++ index.php
@@ -0,0 +1,143 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Google Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Adamina&family=Libre+Franklin&display=swap" rel="stylesheet">
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="./style/index.css">
+
+    <title>Hello, world!</title>
+</head>
+
+<body>
+    <div class="ads">
+
+    </div>
+
+    <nav class="navbar shadow">
+        <div class="navbar-logo">
+            <p>Vague</p>
+        </div>
+        <div class="navbar-links">
+            <a href="#">Title calculator</a>
+            <a href="#" style="color:red">Employees</a>
+        </div> 
+    </nav>
+
+    <main class="main">
+    <form action="welcome_get.php" method="get">
+        <div class="form-title">
+            <p class="display-6">Title Calculator</p>
+        </div>
+
+        <div class="form-subtitle">
+            <p>Employee Information</p>
+        </div>
+
+        <div class="form-row">
+            <div class="form-row-child">
+                <label for="inputPlaceholder3">Placeholder</label>
+                <select class="form-select" aria-label="Default select example" name="val1">
+                <option selected>Select Employee Name</option>
+                <option value="1">One</option>
+                <option value="2">Two</option>
+                <option value="3">Three</option>
+                </select>
+            </div>
+            
+            <div class="form-row-child">
+                <label for="inputPlaceholder3">Placeholder</label>
+                <input type="Placeholder" class="form-control" id="inputPlaceholder3" value="Php 1000.00" readonly name="val2">
+            </div>
+        </div>
+        <div class="form-row">
+            <div class="form-row-child">
+                <label for="inputPlaceholder3">Title</label>
+                <input type="Placeholder" class="form-control" id="inputPlaceholder3" placeholder="Placeholder" name="val3">
+            </div>
+            
+            <div class="form-row-child">
+                <label for="inputPlaceholder3">Absences</label>
+                <input type="Placeholder" class="form-control" id="inputPlaceholder3" placeholder="Placeholder" name="val4">
+            </div>
+        </div>
+
+        <div class="half-form">
+            <div class="half-form-left">
+                <div class="half-form-subtitle">
+                     <p>Employee Information</p>
+                </div>
+                <div class="half-form-child">
+                    <label for="inputPlaceholder3">Title</label>
+                    <input type="Placeholder" class="form-control" id="inputPlaceholder3" placeholder="Placeholder" name="val5">
+                </div>
+                <div class="half-form-child">
+                    <label for="inputPlaceholder3">Title</label>
+                    <input type="Placeholder" class="form-control" id="inputPlaceholder3" placeholder="Placeholder" name="val6">
+                </div>
+                <div class="half-form-child">
+                    <label for="inputPlaceholder3">Title</label>
+                    <input type="Placeholder" class="form-control" id="inputPlaceholder3" placeholder="Placeholder" name="val7">
+                </div>
+            </div>
+            <div class="half-form-right">
+                <div class="half-form-subtitle">
+                     <p>Employee Information</p>
+                </div>
+                <div class="half-form-child">
+                    <label for="inputPlaceholder3">Title</label>
+                    <input type="Placeholder" class="form-control" id="inputPlaceholder3" placeholder="Placeholder" name="val8">
+                </div>
+                <div class="half-form-child">
+                    <label for="inputPlaceholder3">Title</label>
+                    <input type="Placeholder" class="form-control" id="inputPlaceholder3" placeholder="Placeholder" name="val9">
+                </div>
+            </div>
+        </div>
+
+        <div class="half-form">
+        <div class="half-form-left">
+                <div class="half-form-subtitle">
+                     <p>Employee Information</p>
+                </div>
+                <div class="half-form-child">
+                    <label for="inputPlaceholder3">Title</label>
+                    <input type="Placeholder" class="form-control" id="inputPlaceholder3" placeholder="Placeholder" name="val10">
+                </div>
+                <div class="half-form-child">
+                    <label for="inputPlaceholder3">Title</label>
+                    <input type="Placeholder" class="form-control" id="inputPlaceholder3" placeholder="Placeholder" name="val11">
+                </div>
+            </div>
+            <div class="half-form-right-2">
+                <div class="half-form-child-2">
+                    <input type="submit" class="btn btn-danger my-btn">
+                </div>
+            </div>
+        </div>
+
+
+
+        </form>
+    </main>
+
+    <!-- <form action="welcome_get.php" method="get">
+        Name: <input type="text" name="name"><br>
+        E-mail: <input type="text" name="Placeholder"><br>
+        <input type="submit">
+    </form> -->
+
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+</body>
+
+</html>
---
+++ welcome_get.php
@@ -0,0 +1,22 @@
+<html>
+<body>
+
+Value 1: <?php echo $_GET["val1"]; ?><br>
+Value 2: <?php echo $_GET["val2"]; ?><br>
+Value 3: <?php echo $_GET["val3"]; ?><br>
+Value 4: <?php echo $_GET["val4"]; ?><br>
+Value 5: <?php echo $_GET["val5"]; ?><br>
+Value 6: <?php echo $_GET["val6"]; ?><br>
+Value 7: <?php echo $_GET["val7"]; ?><br>
+Value 8: <?php echo $_GET["val8"]; ?><br>
+Value 9: <?php echo $_GET["val9"]; ?><br>
+Value 10: <?php echo $_GET["val10"]; ?><br>
+Value 11: <?php echo $_GET["val11"]; ?><br>
+
+
+</body>
+</html>    <!-- <form action="welcome_get.php" method="get">
+        Name: <input type="text" name="name"><br>
+        E-mail: <input type="text" name="Placeholder"><br>
+        <input type="submit">
+    </form> -->
